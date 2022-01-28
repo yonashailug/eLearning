3940a15aa47c006dca634e7b1afdedf34fd27b2b
aregawihalefom <aregawihalefom90@gmail.com>
2022-01-27 23:21:13 -0600

    config micro-service project created

---
+++ Config/src/test/java/edu/el/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.el.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
