cd571556680898ecd749b8824ad98ac947e37204
aregawihalefom <aregawihalefom90@gmail.com>
2022-01-28 23:57:18 -0600

    progress api added

---
+++ enrollement/src/test/java/edu/hahu/enrollement/EnrollementApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.hahu.enrollement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnrollementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
